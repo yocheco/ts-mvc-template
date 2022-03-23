64fb9f1380f9b0168ef59870bd4bd35158f4ebff
wero <wero@MacBook-Pro-de-wero.local>
2022-03-23 12:48:52 -0600

    :construction: -Add public folder

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from 'express'
+import path from 'path'
 import { Env } from './keys'
 
 // 🤞🏻Init
 const app: Application = express()
 
 // 🛠 Settings
-app.set('port', Env.PORT || 8080)
+app.set('port', Env.PORT)
+
+// 🧬 Middlewares
+app.use(express.static(path.join(__dirname, 'public')))
 
 // 🚀 Start
 app.listen(app.get('port'), () => {
