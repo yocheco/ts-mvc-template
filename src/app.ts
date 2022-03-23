0f9dd9f78142314e754da3bdd5f6d45a9681ad6f
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-03-23 16:26:15 -0600

    :construction: -Add express-handlebars

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import express, { Application } from 'express'
+import { engine } from 'express-handlebars'
 // Import Env
 import { Env } from './keys'
 // Import Routes
@@ -11,6 +12,13 @@ const app: Application = express()
 
 // 🛠 Settings
 app.set('port', Env.PORT)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials')
+}))
+app.set('view engine', '.hbs')
 
 // 🧬 Middlewares
 app.use(express.static(path.join(__dirname, 'public')))
