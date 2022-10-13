f53b13c367a60a2e7c513e0b7ce09ca81132c565
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-10-13 12:50:39 -0500

    :construction: -Add compres images

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const path = require('path')
 const common = require('./webpack.common.js')
 const buildDir = path.resolve(__dirname, 'dist')
 
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
 const serverConfig = {
   target: 'node',
   mode: 'production',
@@ -41,7 +43,92 @@ const clientConfig = merge(common, {
     publicPath: '/frontend/js/',
     chunkFilename: '[name]-chunk.js'
   },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        deleteOriginalAssets: false,
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 7 }],
+              [
+                'svgo',
+                {
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                          addAttributesToSVGElement: {
+                            params: {
+                              attributes: [
+                                { xmlns: 'http://www.w3.org/2000/svg' }
+                              ]
+                            }
+                          }
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]
+            ]
+          }
+        },
+        generator: [
+          {
+            type: 'asset',
+            implementation: ImageMinimizerPlugin.imageminGenerate,
+            options: {
+              plugins: ['imagemin-webp']
+            }
+          }
+        ]
+      })
+    ]
+  },
   plugins: [
+    // new ImageMinimizerPlugin({
+    //   minimizer: {
+    //     implementation: ImageMinimizerPlugin.imageminMinify,
+    //     options: {
+    //       // Lossless optimization with custom option
+    //       // Feel free to experiment with options for better result for you
+    //       plugins: [
+    //         ['gifsicle', { interlaced: true }],
+    //         ['jpegtran', { progressive: true }],
+    //         ['optipng', { optimizationLevel: 5 }],
+    //         // Svgo configuration here https://github.com/svg/svgo#configuration
+    //         [
+    //           'svgo',
+    //           {
+    //             plugins: [
+    //               {
+    //                 name: 'preset-default',
+    //                 params: {
+    //                   overrides: {
+    //                     removeViewBox: false,
+    //                     addAttributesToSVGElement: {
+    //                       params: {
+    //                         attributes: [
+    //                           { xmlns: 'http://www.w3.org/2000/svg' }
+    //                         ]
+    //                       }
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             ]
+    //           }
+    //         ]
+    //       ]
+    //     }
+    //   }
+    // }),
     // ---[Copy files]---
     new CopyPlugin({
       patterns: [
@@ -57,7 +144,12 @@ const clientConfig = merge(common, {
     })
   ],
   module: {
-    rules: []
+    rules: [
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   type: 'asset'
+      // }
+    ]
   }
 })
 
