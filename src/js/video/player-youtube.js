eef92cb19c11acfd59941dadc5c56bac0b3f5e64
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-10-12 19:54:02 -0500

    :recycle: -Refactor files

--- src/js/video/player-youtube.js
+++ src/js/video/player-youtube.js
@@ -1,60 +1,56 @@
 const playObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      const frame = entry.target.children[0];
+      const frame = entry.target.children[0]
 
       if (entry.isIntersecting) {
         console.log('play video ')
-        frame.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-        
+        frame.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*')
 
-        return;
+        return
       }
       console.log('stop video ')
-      frame.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
-    });
+      frame.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
+    })
   },
-  { 
-    threshold: 0.75,
+  {
+    threshold: 0.75
   }
-);
+)
 
 const loadObserver = new IntersectionObserver(
-(entries, observer) => {
+  (entries, observer) => {
     entries.forEach((entry) => {
-      const video = entry.target;
-      const idYoutube = video.dataset.id;
+      const video = entry.target
+      const idYoutube = video.dataset.id
 
       if (entry.isIntersecting) {
         console.log('create new frame')
 
-        const iframe = document.createElement("iframe");
-        iframe.setAttribute("allowfullscreen", "");
+        const iframe = document.createElement('iframe')
+        iframe.setAttribute('allowfullscreen', '')
         iframe.setAttribute(
-          "allow",
-          "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
-        );
+          'allow',
+          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        )
         iframe.setAttribute(
-          "src",
+          'src',
           `https://www.youtube.com/embed/${idYoutube}?rel=0&showinfo=0&autoplay=1&mute=1&enablejsapi=1`
-        );
+        )
 
-        video.innerHTML = "";
-        video.appendChild(iframe);
+        video.innerHTML = ''
+        video.appendChild(iframe)
 
-
-        playObserver.observe(video);
-        observer.unobserve(video);
-
-        return;
+        playObserver.observe(video)
+        observer.unobserve(video)
       }
-    });
+    })
   },
   { threshold: 0 }
-);
+)
 
 const videos = document.querySelectorAll('.js-youtube-player')
 
-export function playerYoutubeStart(){
-  if(videos) videos.forEach(video => loadObserver.observe(video))
+export function playerYoutubeStart () {
+  if (videos) videos.forEach(video => loadObserver.observe(video))
 }
--- src/js/video/player.js
+++ src/js/video/player.js
@@ -1,50 +1,48 @@
 const playObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      const video = entry.target;
+      const video = entry.target
       if (entry.isIntersecting) {
         video.play().catch(() => {
-          console.error("can't play video");
-        });
+          console.error("can't play video")
+        })
 
-        return;
+        return
       }
 
-      video.pause();
-    });
+      video.pause()
+    })
   },
-  { 
+  {
     threshold: 0.75,
-     rootMargin: '-100px 0px 0px 0px',
+    rootMargin: '-100px 0px 0px 0px'
   }
-);
+)
 
 const loadObserver = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        const video = entry.target;
+        const video = entry.target
 
         for (const source in video.children) {
-          const videoSource = video.children[source];
-          if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
-            videoSource.src = videoSource.dataset.src ?? "";
+          const videoSource = video.children[source]
+          if (typeof videoSource.tagName === 'string' && videoSource.tagName === 'SOURCE') {
+            videoSource.src = videoSource.dataset.src ?? ''
           }
         }
 
-        video.load();
-        observer.unobserve(video);
-        playObserver.observe(video);
-
-        return;
+        video.load()
+        observer.unobserve(video)
+        playObserver.observe(video)
       }
-    });
+    })
   },
   { threshold: 0 }
-);
+)
 
 const videos = document.querySelectorAll('.js-video-player')
 
-export function playerStart(){
-  if(videos) videos.forEach(video => loadObserver.observe(video))
+export function playerStart () {
+  if (videos) videos.forEach(video => loadObserver.observe(video))
 }
